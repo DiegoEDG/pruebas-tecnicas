04157e1fcd7134def0d180649e0a5a50484b434d
Digudev <1.5.dg.diego@gmail.com>
2023-07-19 12:34:26 -0600

    FEAT: useFilters Hook Added

---
+++ pruebas/01-reading-list/diegoedg/src/hooks/useFilters.ts
@@ -0,0 +1,23 @@
+import { useContext, useEffect, useState } from 'react';
+import { FiltersContext, StoreContext } from '../context';
+import { Book } from '../interfaces';
+
+export const useFilters = () => {
+	const { genre, pages } = useContext(FiltersContext);
+	const { books } = useContext(StoreContext);
+	const [filteredBooks, setFilteredBooks] = useState<Book[]>(books);
+
+	useEffect(() => {
+		filterBook(genre, pages);
+	}, [genre, pages]);
+
+	const filterBook = (genre: string, pages: number) => {
+		if (genre === '-' && pages >= 40) {
+			setFilteredBooks(books.filter((book) => (book.pages > pages ? book : null)));
+		} else {
+			setFilteredBooks(books.filter((book) => (book.genre === genre && book.pages >= pages ? book : null)));
+		}
+	};
+
+	return { filteredBooks };
+};
