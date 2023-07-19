498c6dad1ff7f569738b053d50ef21b857b6102a
Digudev <1.5.dg.diego@gmail.com>
2023-07-19 08:17:15 -0600

    INIT

--- pruebas/01-reading-list/diegoedg/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as BookCard } from './BookCard';
-export { default as BookList } from './BookList';
---
+++ pruebas/01-reading-list/diegoedg/src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+const App = () => {
+	return (
+		<div className={styles.mainContainer}>
+			<h1>Library App</h1>
+		</div>
+	);
+};
+
+export default App;
