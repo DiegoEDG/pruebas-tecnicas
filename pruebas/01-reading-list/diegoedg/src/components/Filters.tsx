94ca7812c88f461265dcbbb4353a915c3e2252b9
Digudev <1.5.dg.diego@gmail.com>
2023-07-19 12:37:27 -0600

    FEAT: Filters Component added

--- pruebas/01-reading-list/diegoedg/src/components/Filters.tsx
+++ pruebas/01-reading-list/diegoedg/src/components/Filters.tsx
@@ -1,5 +1,28 @@
+import { useContext } from 'react';
+import { FiltersContext } from '../context';
+import { genres } from '../data/genres';
+import styles from './Filters.module.css';
+
 const Filters = () => {
-	return <h1>Filters</h1>;
+	const { handleGenre, handleRange, pages } = useContext(FiltersContext);
+
+	return (
+		<section className={styles.filtersContainer}>
+			<div className={styles.selectContainer}>
+				<label htmlFor="genreFilter">Genre: </label>
+				<select id="genreFilter" className={styles.select} onChange={handleGenre}>
+					{genres.map((genre) => (
+						<option key={genre}>{genre}</option>
+					))}
+				</select>
+			</div>
+			<div className={styles.rangeContainer}>
+				<label htmlFor="pagesFilter">Pages: </label>
+				<input type="range" id="pagesFilter" min="40" max="1200" onChange={handleRange} />
+				<p>{pages}</p>
+			</div>
+		</section>
+	);
 };
 
 export default Filters;
