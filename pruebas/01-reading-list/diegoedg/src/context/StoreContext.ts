8d50e697305f39fed058583b059fbe43487b6084
Digudev <1.5.dg.diego@gmail.com>
2023-07-19 09:19:47 -0600

    CHORES: Context SetUp

---
+++ pruebas/01-reading-list/diegoedg/src/context/StoreContext.ts
@@ -0,0 +1,9 @@
+import { createContext } from 'react';
+import { Book } from '../interfaces';
+
+export interface StoreContextProps {
+	books: Book[];
+	list: Book[];
+}
+
+export const StoreContext = createContext({} as StoreContextProps);
---
+++ pruebas/01-reading-list/diegoedg/src/context/index.ts
@@ -0,0 +1,3 @@
+export * from './StoreContext';
+export { default as StoreProvider } from './StoreProvider';
+export { default as StoreReducer } from './StoreReducer';
