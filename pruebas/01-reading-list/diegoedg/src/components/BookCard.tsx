5bfef5ec051c8635fa2e95b882b3d9b9e962eac4
Digudev <1.5.dg.diego@gmail.com>
2023-07-19 08:19:35 -0600

    INIT: Basic Components Added

---
+++ pruebas/01-reading-list/diegoedg/src/components/BookCard.tsx
@@ -0,0 +1,5 @@
+const BookCard = () => {
+	return <h1>BookCard</h1>;
+};
+
+export default BookCard;
---
+++ pruebas/01-reading-list/diegoedg/src/components/BookList.tsx
@@ -0,0 +1,12 @@
+import BookCard from './BookCard';
+
+const BookList = () => {
+	return (
+		<div>
+			<h1>BookList</h1>
+			<BookCard />
+		</div>
+	);
+};
+
+export default BookList;
