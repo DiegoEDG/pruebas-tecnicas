860c416ae176662cf2067dcbe3f78dc8563f2ae8
Digudev <1.5.dg.diego@gmail.com>
2023-07-19 10:38:35 -0600

    UI: Icon added

--- pruebas/01-reading-list/diegoedg/src/App.tsx
+++ pruebas/01-reading-list/diegoedg/src/App.tsx
@@ -1,9 +1,20 @@
 import styles from './App.module.css';
+import { BookList } from './components';
+import listIcon from './assets/list.svg';
 
 const App = () => {
 	return (
 		<div className={styles.mainContainer}>
-			<h1>Library App</h1>
+			<div className={styles.header}>
+				<h1>Library App</h1>
+				<div className={styles.header}>
+					<h4 className={styles.listCounter}>List (5)</h4>
+					<button className={styles.iconButton}>
+						<img src={listIcon} style={{ width: 40, height: 40 }} />
+					</button>
+				</div>
+			</div>
+			<BookList />
 		</div>
 	);
 };
